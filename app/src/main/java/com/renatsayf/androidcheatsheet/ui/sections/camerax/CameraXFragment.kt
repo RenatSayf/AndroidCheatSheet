477e8ea2b86551aa7b23b633d8e3157338fb93d0
Renat <renatsayf@gmail.com>
2022-07-24 23:47:35 +0500

    CameraXFragment.kt take photo has been implemented

--- app/src/main/java/com/renatsayf/androidcheatsheet/ui/sections/camerax/CameraXFragment.kt
+++ app/src/main/java/com/renatsayf/androidcheatsheet/ui/sections/camerax/CameraXFragment.kt
@@ -1,21 +1,55 @@
+@file:Suppress("ObjectLiteralToLambda")
+
 package com.renatsayf.androidcheatsheet.ui.sections.camerax
 
-import androidx.lifecycle.ViewModelProvider
+import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.renatsayf.androidcheatsheet.R
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModelProvider
+import com.renatsayf.androidcheatsheet.BuildConfig
 import com.renatsayf.androidcheatsheet.databinding.FragmentCameraXBinding
+import com.renatsayf.androidcheatsheet.ui.sections.extentions.showSnackBar
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CameraXFragment : Fragment() {
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
 
     private lateinit var binding: FragmentCameraXBinding
 
     private val cameraVM: CameraXViewModel by lazy {
         ViewModelProvider(this)[CameraXViewModel::class.java]
     }
+
+    private var imageCapture: ImageCapture? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +58,183 @@ class CameraXFragment : Fragment() {
         binding = FragmentCameraXBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val isGranted = allPermissionsGranted()
+        if (isGranted) {
+            startCamera().observe(viewLifecycleOwner) {
+                imageCapture = it
+                if (imageCapture is ImageCapture) {
+                    cameraVM.setState(CameraXViewModel.State.OnReady)
+                }
+                else cameraVM.setState(CameraXViewModel.State.NotReady)
+            }
+        }
+        else {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        with(binding) {
+
+            cameraVM.state.observe(viewLifecycleOwner) { state ->
+                when(state) {
+                    CameraXViewModel.State.NotReady -> {
+                        imageCaptureButton.isEnabled = false
+                        videoCaptureButton.isEnabled = false
+                        btnZoomPlus.isEnabled = false
+                        btnZoomMinus.isEnabled = false
+                    }
+                    CameraXViewModel.State.OnReady -> {
+                        imageCaptureButton.isEnabled = true
+                        videoCaptureButton.isEnabled = true
+                        btnZoomPlus.isEnabled = true
+                        btnZoomMinus.isEnabled = true
+                    }
+                    CameraXViewModel.State.OnPhotoCompleted -> {
+                        progressBar.visibility = View.GONE
+                    }
+                    CameraXViewModel.State.OnStartPhoto -> {
+                        progressBar.visibility = View.VISIBLE
+                    }
+                }
+            }
+
+            imageCaptureButton.setOnClickListener {
+                imageCapture?.let {
+                    cameraVM.setState(CameraXViewModel.State.OnStartPhoto)
+                    takePhoto(it)
+                }
+            }
+
+            btnZoomPlus.setOnClickListener {
+                imageCapture?.let { ic -> zoomPlus(ic, value = 0.25F) }
+            }
+
+            btnZoomMinus.setOnClickListener {
+                imageCapture?.let { ic -> zoomMinus(ic, value = -0.25f) }
+            }
+        }
+
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            val isGranted = allPermissionsGranted()
+            if (isGranted) {
+                startCamera().observe(viewLifecycleOwner) {
+                    imageCapture = it
+                    if (imageCapture is ImageCapture) {
+                        cameraVM.setState(CameraXViewModel.State.OnReady)
+                    }
+                    else cameraVM.setState(CameraXViewModel.State.NotReady)
+                }
+            }
+            else {
+                showSnackBar("Permissions not granted by the user.")
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera(): LiveData<ImageCapture?> {
+
+        val imageCapture = MutableLiveData<ImageCapture>(null)
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener(object : Runnable {
+
+            override fun run() {
+
+                val cameraProvider = cameraProviderFuture.get()
+                val preview = Preview.Builder()
+                    .build().also {
+                        it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                    }
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+                try {
+                    imageCapture.value = ImageCapture.Builder().build()
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        this@CameraXFragment,
+                        cameraSelector,
+                        preview,
+                        imageCapture.value
+                    )
+
+                } catch (e: Exception) {
+                    imageCapture.value = null
+                    showSnackBar("Use case binding failed")
+                    if (BuildConfig.DEBUG) e.printStackTrace()
+                }
+            }
+        }, requireActivity().mainExecutor)
+        return imageCapture
+    }
+
+    private fun takePhoto(imageCapture: ImageCapture) {
+
+        val fileName = SimpleDateFormat(
+            FILENAME_FORMAT,
+            Locale.getDefault()
+        ).format(System.currentTimeMillis())
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        val outPutOptions = ImageCapture.OutputFileOptions.Builder(
+            requireContext().contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        ).build()
+
+        imageCapture.takePicture(
+            outPutOptions,
+            ContextCompat.getMainExecutor(requireContext()),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    cameraVM.setState(CameraXViewModel.State.OnPhotoCompleted)
+                    val msg = "Photo capture succeeded: ${outputFileResults.savedUri}"
+                    showSnackBar(msg)
+                }
+                override fun onError(exception: ImageCaptureException) {
+                    cameraVM.setState(CameraXViewModel.State.OnPhotoCompleted)
+                    showSnackBar("Photo capture failed: ${exception.message}")
+                }
+            }
+        )
+    }
+
+    private fun zoomMinus(imageCapture: ImageCapture, value: Float = -1.0f) {
+        zoomPlus(imageCapture, value)
+    }
+
+    private fun zoomPlus(imageCapture: ImageCapture, value: Float = 1.0f) {
+        val camera = imageCapture.camera
+        val zoomState = camera?.cameraInfo?.zoomState?.value
+        val maxZoomRatio = zoomState?.maxZoomRatio ?: 1.0f
+        val minZoomRatio = zoomState?.minZoomRatio ?: 0.0f
+        val zoomRatio = zoomState?.zoomRatio ?: 0.0f
+        val newRatio = zoomRatio + value
+        if (newRatio in minZoomRatio..maxZoomRatio) {
+            camera?.cameraControl?.setZoomRatio(newRatio)
+        }
+    }
+
+
 
 
 
--- app/src/main/java/com/renatsayf/androidcheatsheet/ui/sections/camerax/CameraXViewModel.kt
+++ app/src/main/java/com/renatsayf/androidcheatsheet/ui/sections/camerax/CameraXViewModel.kt
@@ -1,7 +1,23 @@
 package com.renatsayf.androidcheatsheet.ui.sections.camerax
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
-class CameraXViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class CameraXViewModel(private val app: Application) : AndroidViewModel(app) {
+
+    sealed class State {
+        object NotReady: State()
+        object OnReady: State()
+        object OnStartPhoto: State()
+        object OnPhotoCompleted: State()
+    }
+
+    private var _state = MutableLiveData<State>(State.NotReady)
+    val state: LiveData<State> = _state
+    fun setState(state: State) {
+        _state.value = state
+    }
+
 }
