3bc01a4df554c170ce9611198952cbce93d6c8ba
Renat <renatsayf@gmail.com>
2022-07-24 15:16:10 +0500

    build.gradle has been updated

--- app/src/main/java/com/renatsayf/androidcheatsheet/models/DeepLinks.kt
+++ app/src/main/java/com/renatsayf/androidcheatsheet/models/DeepLinks.kt
@@ -5,6 +5,7 @@ object DeepLinks {
     val links = mapOf(
         "https://github.com/RenatSayf/AndroidCheatSheet/blob/master/sections/recycler_view/Compose%20example.md" to "app://recycler_view/compose_list",
         "https://github.com/RenatSayf/AndroidCheatSheet/blob/master/sections/recycler_view/Simple%20example.md" to "app://recycler_view/simple_list",
-        "https://github.com/RenatSayf/AndroidCheatSheet/blob/master/sections/exoplayer/Exoplayer.md" to "app://exo_player"
+        "https://github.com/RenatSayf/AndroidCheatSheet/blob/master/sections/exoplayer/Exoplayer.md" to "app://exo_player",
+        "https://github.com/RenatSayf/AndroidCheatSheet/blob/master/sections/CameraX.md" to "app://camera_x"
     )
 }
